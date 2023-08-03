ddfb7f0a5ed6cf01587c7c4b99a09bf89b5d6f9b
Jidraph! <jidraphkimachia1@gmail.com>
2023-08-03 14:20:35 +0300

    backend configuration

--- Routes/projectRoutes.js
+++ Routes/projectRoutes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { createProject, getProjects, getOneProject, updateProject, deleteProject } = require('../Controllers/ProjectsController');
+const { createProject, getProjects, getOneProject, updateProject, deleteProject } = require('../Controllers/projectsController');
 const { verifyToken } = require('../Middleware/verifyToken');
 
 const projectrouter = Router();
